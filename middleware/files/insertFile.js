e7c0a7f1f188f3ec9c13a177140a55010813c57a
i-am-pepsiman <mani-designs@hotmail.com>
2023-12-07 20:00:09 +0100

    Added middleware for Demands, created test routes, modified index.middleware, renamed file due to typo

---
+++ middleware/files/insertFile.js
@@ -0,0 +1,15 @@
+import insertFile from '../../controllers/demands.controller.js';
+
+const main = async (req, res, next) => {
+    try {
+        await insertFile(req.body.file);
+        res.send({
+            status: 200,
+            message: `File ${req.body.file} inserted successfully.`
+        })
+    } catch (error) {
+        next(error);
+    }
+};
+
+export default main;
