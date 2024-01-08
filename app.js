b48f2c8bfa3da8eebfac373d4fa84d761ad0bd55
Abraham <228.abraham.diaz@gmail.com>
2024-01-08 18:09:01 +0000

    funcionando

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ import notFoundMiddleware from './middlewares/security/notFound.middleware.js';
 import errorHandlerMiddleware from './middlewares/security/errorHandler.middleware.js';
 
 dotenv.config();
+const PORT = process.env.PORT
 
 const app = express();
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 
 
