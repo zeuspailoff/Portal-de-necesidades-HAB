7b25a07f1eda627e9732707518ae62a9f046f94b
Abraham <228.abraham.diaz@gmail.com>
2024-01-15 22:16:02 +0000

    arreglamos bug init_DB

--- helpers/mailer.helper.js
+++ helpers/mailer.helper.js
@@ -1,29 +1,31 @@
 import sendMail from '../helpers/sendMail.helper.js';
+import dotenv from 'dotenv';
 
+const BASE_URL = process.env.BASE_URL;
+const PORT = process.env.PORT;
 
-
-export const mailToRegistration = async (username,email, registrationCode) =>{
+export const mailToRegistration = async (username, email, registrationCode) => {
     const emailBody =
         `Bienvenido ${username}
     Gracias por registrarte en Portal de necesidades. Para activar tu cuenta, haz clic en el siguiente enlace:
     
     Click aqui para activar tu cuenta de PORTAL DE NECESIDADES: 
             
-                    "http://localhost:8080/users/validate/${registrationCode}" 
+                    "${BASE_URL}:${PORT}/users/validate/${registrationCode}" 
     `
-   await sendMail(email, `Activa tu cuenta`, emailBody);
+    await sendMail(email, `Activa tu cuenta`, emailBody);
 }
 
-export const mailToRecoverPassword = async (username,email, passwordRecovery) =>{
+export const mailToRecoverPassword = async (username, email, passwordRecovery) => {
     const emailBody =
         `Hola ${username}
     Si usted no ha solicitado este correo, ignorelo.
     
     Para recuperar tu contraseña, envia un request PUT con el campo "password" a la siguiente URL:
-    "http://localhost:8080/users/SetPassByrecover/${passwordRecovery}"
+    "${BASE_URL}:${PORT}/users/SetPassByrecover/${passwordRecovery}"
     
     `
 
-   await sendMail(email, `RECUPERAR PASSWORD`, emailBody);
+    await sendMail(email, `RECUPERAR PASSWORD`, emailBody);
 }
 
--- schemas/users/updateUser.schema.js
+++ schemas/users/updateUser.schema.js
@@ -7,7 +7,7 @@ const updateUserSchema = joi.object({
     lastname: joi.string().min(1).max(50).required().messages(joiMsg),
     username: joi.string()
         .min(3)
-        .max(30)
+        .max(50)
         .required()
         .pattern(/^\S+$/)
         .messages({ ...joiMsg.errorMsg, ...joiMsg.errorMsgUsername }),
@@ -18,8 +18,7 @@ const updateUserSchema = joi.object({
     email: joi.string().email().required().messages(joiMsg),
     biography: joi.string().min(5).max(1000).allow('').messages(joiMsg),
     birthdate: joi.date().iso().required().messages(joiMsg),
-    phone: joi.string().min(5).max(15).required().messages(joiMsg),
-    files: joi.array().allow('').items(joi.string().min(10).max(1000).messages(joiMsg)),
+    phone: joi.string().min(5).max(20).required().messages(joiMsg),
 });
 
 export default updateUserSchema;
